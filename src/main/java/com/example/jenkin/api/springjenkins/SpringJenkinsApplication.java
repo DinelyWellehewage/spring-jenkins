3cdc906977b40aba3c074927fc8b5bc97b30f5ac
Dinely_105988 <dinely.wellehewage@axiatadigitallabs.com>
2023-11-24 21:35:03 +0100

    first commit

---
+++ src/main/java/com/example/jenkin/api/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.jenkin.api.springjenkins;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@PostConstruct
+	public void initt(){
+		logger.info("application started ...");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
