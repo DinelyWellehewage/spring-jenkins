7952ae27ed263252263b1bfe35442d2007b6e688
DinelyWellehewage <dinelywh78@gmail.com>
2023-11-24 22:03:36 +0100

    first 2 commit

--- src/test/java/com/example/jenkin/api/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/jenkin/api/springjenkins/SpringJenkinsApplicationTests.java
@@ -2,11 +2,10 @@ package com.example.jenkin.api.springjenkins;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
-	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
 	@Test
 	void contextLoads() {
